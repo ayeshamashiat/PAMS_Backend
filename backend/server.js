a44b546e5ee9b8db60e9a06111cfdb0254e22ff7
ayeshamashiat <ayesha.mashiat101@gmail.com>
2025-07-16 10:23:05 +0600

    port

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const cors = require("cors");
 
 dotenv.config();
 const app = express();
-const PORT = 3000;
+const PORT = 5000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true })); 
