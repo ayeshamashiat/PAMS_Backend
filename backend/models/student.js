1a1f4349f3ce89f692ae39086db463c007f90ae9
ayeshamashiat <ayesha.mashiat101@gmail.com>
2025-07-28 15:18:33 +0600

    program id

--- backend/models/student.js
+++ backend/models/student.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const studentSchema = new mongoose.Schema({
   user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true, unique: true },
   student_number: { type: String, required: true, unique: true },
-  program_id: { type: String, required: true, unique: true },
+  program_id: { type: String, required: true },
   admission_year: { type: Number, required: true },
   current_semester: { type: Number, default: 1 },
   cgpa: { type: Number, default: 0.0 },
