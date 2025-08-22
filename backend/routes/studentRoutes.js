541d3776566b1da86b4d4f2e4ec88eeea63dd71b
ayeshamashiat <ayesha.mashiat101@gmail.com>
2025-08-22 21:03:40 +0600

    Added thesis submission for student

--- backend/routes/studentRoutes.js
+++ backend/routes/studentRoutes.js
@@ -1,40 +1,30 @@
 const express = require('express');
 const Student = require('../models/student');
 const router = express.Router();
-const {getStudentProfile, getStudentProgress, getStudentCourses} = require('../controllers/studentController');
-const { protect } = require('../middleware/authMiddleware');
-const supervisorAssignmentRoutes = require('./supervisorAssignmentRoutes'); // Add this line
+const {getStudentProfile, submitThesisProposal, getStudentProgress, getStudentCourses, getStudentById} = require('../controllers/studentController');
+const supervisorAssignmentRoutes = require('./supervisorAssignmentRoutes'); 
+const {protect} = require('../middleware/authMiddleware');
+const upload = require('../middleware/upload');
 
 //must be before :id
-router.get('/profile', protect, getStudentProfile);
+router.get('/profile', getStudentProfile);
 router.get('/progress', getStudentProgress);
 router.get('/courses', protect, getStudentCourses);
 
-// router.post('/', async (req, res) => {
-//   try {
-//     const student = new Student(req.body);
-//     await student.save();
-//     res.json({ message: 'Student created', student });
-//   } catch (error) {
-//     res.status(500).json({ error: error.message });
-//   }
-// });
-
-router.get('/:id', async (req, res) => {
+router.post('/', async (req, res) => {
   try {
-    const student = await Student.findById(req.params.id)
-      .populate('userId')
-      .populate('department')
-      .exec();
-    if (!student) return res.status(404).json({ error: 'Student not found' });
-    res.json(student);
+    const student = new Student(req.body);
+    await student.save();
+    res.json({ message: 'Student created', student });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
 });
 
-//do a getStduentByID
+router.get('/:id', getStudentById);
 
 router.use('/supervisor-assignment', supervisorAssignmentRoutes);
+
+router.post('/submit', upload.single('attachment'), submitThesisProposal);
 
 module.exports = router;
