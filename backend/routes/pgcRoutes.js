17a9e4bf48060f6ee71933031d1ab50b097041d7
Ayesha Mashiat <ayeshamashiat@iut-dhaka.edu>
2025-10-19 10:40:53 +0600

    updated pgcRoutes

--- backend/routes/pgcRoutes.js
+++ backend/routes/pgcRoutes.js
@@ -9,17 +9,24 @@ const {
   getPGCProfile,
   getPendingProposals,
   getApprovedProposals,
-
+  getPGCManualAssign,
+  getAvailableSupervisors,
+  assignSupervisorManually,
+  getPGCManualAssignmentsWithSupervisors
 } = require("../controllers/pgcController");
 const { protect } = require("../middleware/authMiddleware");
 
 router.post("/pgc-respond", pgcRespond);
 router.post("/pgc-manual-assign", pgcManualAssign);
 router.post("/pgc-review", pgcReviewProposal);
-router.get("/supervision-requests", protect, getPGCSupervisionRequests);
-router.get("/assigned-supervisors", protect, getPGCAssignedSupervisors);
+router.get("/supervision-requests", getPGCSupervisionRequests);
+router.get("/assigned-supervisors", getPGCAssignedSupervisors);
 router.get("/profile", protect, getPGCProfile);
 router.get('/pending-proposals', getPendingProposals);
 router.get('/approved-proposals', getApprovedProposals); 
+router.get('/manual-assign-students', getPGCManualAssign);
 router.post('/review-proposal', pgcReviewProposal);
+router.get('/available-supervisors', getAvailableSupervisors);
+router.post('/assign-supervisor', assignSupervisorManually);
+router.get('/manual-assignments-with-supervisors', getPGCManualAssignmentsWithSupervisors);
 module.exports = router;
